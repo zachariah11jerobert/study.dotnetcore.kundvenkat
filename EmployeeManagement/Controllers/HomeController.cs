015164d57a0eab723e75e798efde2d4de6c67aa1
jerome <zachariah11jerobert@gmail.com>
2023-11-18 21:52:04 +0530

    learned about viewBag

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -24,8 +24,8 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            ViewData["Employee"] = model;
-            ViewData["PageTitle"] = "Employee Details";
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "Employee Details";
             return View(model);
         }
 
