b360a521da37e470b80cb1c098da445e75b44acf
jerome <zachariah11jerobert@gmail.com>
2023-11-18 16:47:04 +0530

    learn about json data in controller

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -9,16 +9,22 @@ namespace EmployeeManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private IEmployeeRepository _employeeRepository;
+        private readonly IEmployeeRepository _employeeRepository;
 
         public HomeController(IEmployeeRepository employeeRepository)
         {
             _employeeRepository = employeeRepository;
         }
 
-        private string Index()
+        public string Index()
         {
             return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        public JsonResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return Json(model);
         }
     }
 }
