5696d9f416880e1d3871d8dfe0fa61550100d903
jerome <zachariah11jerobert@gmail.com>
2023-11-18 20:55:27 +0530

    learn about various ways to use View Pages

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -21,10 +21,25 @@ namespace EmployeeManagement.Controllers
             return _employeeRepository.GetEmployee(1).Name;
         }
 
-        public JsonResult Details()
+        public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            return Json(model);
+            return View(model);
+        }
+
+        public ViewResult TestPage()
+        {
+            return View("Test");
+        }
+
+        public ViewResult TestAbsolutePathPage()
+        {
+            return View("MyViews/Test.cshtml");
+        }
+
+        public ViewResult TestRelativePathPage()
+        {
+            return View("../Test/Update");
         }
     }
 }
