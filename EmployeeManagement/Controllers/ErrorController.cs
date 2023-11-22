206ec3b616dc568d337cbf3309b8f8b4fbb2376a
jerome <zachariah11jerobert@gmail.com>
2023-11-23 00:07:28 +0530

    Learned about centralized Error Page

---
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    break;
+            }
+            return View("NotFound");
+        }
+    }
+}
