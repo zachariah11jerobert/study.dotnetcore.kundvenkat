200467b1b053c65167478163d05ab237e8ff0555
jerome <zachariah11jerobert@gmail.com>
2023-11-22 11:17:33 +0530

    learned about EF core setup and Initail Migrations

---
+++ EmployeeManagement/Models/SQLEmployeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class SQLEmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext context;
+
+        public SQLEmployeeRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Employee Add(Employee employee)
+        {
+            this.context.Employees.Add(employee);
+            this.context.SaveChanges();
+            return employee;
+        }
+
+        public Employee Delete(int id)
+        {
+            Employee employee = this.context.Employees.Find(id);
+            if (employee != null)
+            {
+                this.context.Employees.Remove(employee);
+                this.context.SaveChanges();
+            }
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return this.context.Employees;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return this.context.Employees.Find(Id);
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            var employee = this.context.Employees.Attach(employeeChanges);
+            employee.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            this.context.SaveChanges();
+            return employeeChanges;
+        }
+    }
+}
--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -7,19 +7,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EmployeeManagement
 {
     public class Startup
+
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
-            //services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
-            //services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+            services.AddMvc().AddXmlSerializerFormatters();
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
